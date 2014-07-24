5a196fbdaef7203848a83cfa8220cb1376cb3e71
ashish belokar <ashish_belokar@persistent.co.in>
2014-07-24 19:24:45 +0530

    Update SparkConsumerAMQP.java

--- data-sync-consumer/src/main/java/com/dla/foundation/services/queue/consumer/SparkConsumerAMQP.java
+++ data-sync-consumer/src/main/java/com/dla/foundation/services/queue/consumer/SparkConsumerAMQP.java
@@ -14,6 +14,7 @@ import com.dla.foundation.data.entities.analytics.AnalyticsCollectionEvent;
 import com.dla.foundation.services.queue.updater.CassandraUpdater;
 import com.dla.foundation.services.queue.updater.ElasticSearchUpdater;
 import com.dla.foundation.services.queue.updater.PIOUpdater;
+import com.dla.foundation.services.queue.updater.EmailUpdater;
 import com.dla.foundation.services.queue.updater.Updater;
 import com.dla.foundation.services.queue.util.QueueListenerConfigHandler;
 import com.dla.foundation.services.queue.util.QueueListenerConfigHandler.QueueConfig;
