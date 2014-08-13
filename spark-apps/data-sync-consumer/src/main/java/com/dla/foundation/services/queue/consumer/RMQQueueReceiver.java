d8e3cb3ad46376127d69aca0e66da78ac225e6eb
Tushar Sudake <tsudake.psl@dlavideo.com>
2014-08-13 17:34:55 +0530

    Updaters are now initialized using relfection

--- spark-apps/data-sync-consumer/src/main/java/com/dla/foundation/services/queue/consumer/RMQQueueReceiver.java
+++ spark-apps/data-sync-consumer/src/main/java/com/dla/foundation/services/queue/consumer/RMQQueueReceiver.java
@@ -2,15 +2,13 @@ package com.dla.foundation.services.queue.consumer;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.apache.spark.storage.StorageLevel;
 import org.apache.spark.streaming.receiver.Receiver;
 
 import com.dla.foundation.data.entities.analytics.AnalyticsCollectionEvent;
-import com.dla.foundation.services.queue.updater.CassandraUpdater;
-import com.dla.foundation.services.queue.updater.ElasticSearchUpdater;
-import com.dla.foundation.services.queue.updater.EmailUpdater;
-import com.dla.foundation.services.queue.updater.PIOUpdater;
 import com.dla.foundation.services.queue.updater.Updater;
 import com.dla.foundation.services.queue.util.QueueListenerConfigHandler;
 import com.dla.foundation.services.queue.util.QueueListenerConfigHandler.QueueConfig;
@@ -21,7 +19,7 @@ public class RMQQueueReceiver extends Receiver<AnalyticsCollectionEvent> impleme
 	 * 
 	 */
 	private static final long serialVersionUID = 1180932706137696729L;
-	
+
 	private QueueListenerConfigHandler qConfig;
 
 	public RMQQueueReceiver(StorageLevel storageLevel) {
@@ -37,19 +35,26 @@ public class RMQQueueReceiver extends Receiver<AnalyticsCollectionEvent> impleme
 	public void onStart() {
 		Updater updater = null;
 		for (QueueConfig oneQConfig : qConfig.getqConfigs()) {
-			if(oneQConfig.getUpdater().equalsIgnoreCase("PIOUpdater"))
-				updater = new PIOUpdater();
-			else if(oneQConfig.getUpdater().equalsIgnoreCase("ElasticSearchUpdater"))
-				updater = ElasticSearchUpdater.getInstance();
-			else if(oneQConfig.getUpdater().equalsIgnoreCase("CassandraUpdater"))
-				updater = new CassandraUpdater();
-			else if (oneQConfig.getUpdater().equalsIgnoreCase("EmailUpdater"))
-				updater = new EmailUpdater();
+			Class<? extends Updater> updaterClass = null;
+			try {
+				updaterClass = (Class<Updater>) Class.forName(oneQConfig.getUpdater());
+				updater = updaterClass.newInstance();
+			} catch (IllegalAccessException e) {
+				try {
+					Method instanceMthd = updaterClass.getMethod("getInstance", null);
+					updater = (Updater) instanceMthd.invoke(null, null);
 
-			if(oneQConfig.getType()==QueueListenerConfigHandler.queue_type.sync)
-				new Thread(new SyncQueueConsumer(oneQConfig,updater)).start();
-			else if(oneQConfig.getType()==QueueListenerConfigHandler.queue_type.async)
-				new Thread(new AsyncQueueConsumer(oneQConfig,updater)).start();
+					if(oneQConfig.getType()==QueueListenerConfigHandler.queue_type.sync)
+						new Thread(new SyncQueueConsumer(oneQConfig,updater)).start();
+					else if(oneQConfig.getType()==QueueListenerConfigHandler.queue_type.async)
+						new Thread(new AsyncQueueConsumer(oneQConfig,updater)).start();
+				} catch (NoSuchMethodException | SecurityException | IllegalAccessException 
+						| IllegalArgumentException | InvocationTargetException e1) {
+					e1.printStackTrace();
+				}
+			} catch (ClassNotFoundException | InstantiationException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
