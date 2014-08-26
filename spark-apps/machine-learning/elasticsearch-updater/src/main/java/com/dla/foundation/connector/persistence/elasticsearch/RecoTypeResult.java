db2c3581975e270d2da5662e3a99481f1320665d
manasee <manasee_kamble@persistent.co.in>
2014-08-26 19:31:44 +0530

    Changes include : 1. Maintaining JSON doc at catalog/reco_type/_show. This doc is referred during delete $

---
+++ spark-apps/machine-learning/elasticsearch-updater/src/main/java/com/dla/foundation/connector/persistence/elasticsearch/RecoTypeResult.java
@@ -0,0 +1,26 @@
+package com.dla.foundation.connector.persistence.elasticsearch;
+
+import com.dla.foundation.connector.model.RecoType;
+
+
+public class RecoTypeResult {
+	
+
+		public String _index;
+		public String _type;
+		public String _id;
+		public long _version;
+		public boolean found;
+		
+		public RecoType _source;
+
+		@Override
+		public String toString() {
+			return "RecoTypeResult [_id=" + _id + ", _index=" + _index
+					+ ", _source=" + _source.toString() + ", _type=" + _type + ", _version="
+					+ _version + ", found=" + found + "]";
+		}
+		
+		
+	
+}
---
+++ spark-apps/machine-learning/elasticsearch-updater/src/main/java/com/dla/foundation/connector/util/PropKeys.java
@@ -0,0 +1,33 @@
+package com.dla.foundation.connector.util;
+
+public enum PropKeys {
+
+	MODE_PROPERTY("spark_mode"), APP_NAME("app_name"), INPUT_KEYSPACE(
+			"cs_fisKeyspace"), INPUT_COLUMNFAMILY("input_columnfamily"), INPUT_PARTITIONER(
+			"input_partitioner"), INPUT_RPC_PORT("cs_rpcPort"), OUTPUT_KEYSPACE(
+			"output_keyspace"), OUTPUT_COLUMNFAMILY("output_columnfamily"), OUTPUT_PARTITIONER(
+			"output_partitioner"), PAGE_ROW_SIZE("cs_pageRowSize"), EVENT_REQUIRED(
+			"event_required"), INPUT_DATE("input_date"), INPUT_HOST_LIST(
+			"cs_hostList"), OUTPUT_HOST_LIST("cs_hostList"), RECAL_START_DATE(
+			"recalculation_start_date"), RECAL_END_DATE(
+			"recalculation_end_date"), RECAL_PERIOD("recalculation_period"), INCREMENTAL_FLAG(
+			"incremental_flag"), ZSCORE_PERIOD("zscore_period"), CURRENT_TREND_DATE(
+			"current_trend_date"), FLAG("flag"), DATE("date"), FULL_QUALIFIED_CLASSNAME(
+			"fully_qualified_classname"),ES_HOST("es_host"),ES_PORT("es_httpPort");
+
+	private String value;
+
+	PropKeys(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
+
+}
