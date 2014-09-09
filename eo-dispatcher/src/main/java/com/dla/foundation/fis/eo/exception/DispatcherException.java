a1b1d63147856b23e79d4125ae609860147e4608
Tushar Sudake <tsudake.psl@dlavideo.com>
2014-09-09 16:23:39 +0530

    Deleted unused eo-dispatcher library. It's now moved to platform

--- eo-dispatcher/src/main/java/com/dla/foundation/fis/eo/exception/DispatcherException.java
+++
@@ -1,9 +0,0 @@
-package com.dla.foundation.fis.eo.exception;
-
-public class DispatcherException extends Exception{
-
-	private static final long serialVersionUID = -4980857388675571495L;
-	public DispatcherException(String message) {
-		super(message);
-	}
-}
