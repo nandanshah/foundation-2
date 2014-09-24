3c1efced243e311204f52aa9aef22f37e342325b
Tushar Sudake <tsudake.psl@dlavideo.com>
2014-09-24 20:39:24 +0530

    Restored currentRegionId

--- spark-apps/data-sync-consumer/src/main/java/com/dla/foundation/intelligence/entity/UserEvent.java
+++ spark-apps/data-sync-consumer/src/main/java/com/dla/foundation/intelligence/entity/UserEvent.java
@@ -121,8 +121,7 @@ public class UserEvent extends SimpleFoundationEntity {
 		userEvent.purchaseStartTimestamp = strToLong(ue.startTimestamp);
 		userEvent.purchaseTimestamp = strToLong(ue.startTimestamp);
 		userEvent.rateScore = strToInt(ue.rateScore);
-		//userEvent.regionID = strToUUID(ue.currentRegionId);
-		userEvent.regionID = strToUUID(ue.homeRegionId);
+		userEvent.regionID = strToUUID(ue.currentRegionId);
 		userEvent.rentEndTimestamp = strToLong(ue.endTimestamp);
 		userEvent.rentStartTimestamp = strToLong(ue.startTimestamp);
 		//userEvent.useraction = ue.action;
