b10d6ee6f298256741d09fb4bddd9917c1dc71a8
Sachin Gaikwad <sachin_gaikwad@persistent.co.in>
2014-09-24 18:17:29 +0530

    Consumer changes for architecture changes in rabbit-mq

--- spark-apps/data-sync-consumer/src/main/java/com/dla/foundation/intelligence/eo/consumer/AsyncQueueConsumer.java
+++ spark-apps/data-sync-consumer/src/main/java/com/dla/foundation/intelligence/eo/consumer/AsyncQueueConsumer.java
@@ -8,17 +8,18 @@ import com.dla.foundation.data.entities.event.Event;
 import com.dla.foundation.intelligence.eo.updater.Updater;
 import com.dla.foundation.intelligence.eo.util.BlockedListenerLogger;
 import com.dla.foundation.intelligence.eo.util.QueueListenerConfigHandler.QueueConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
 
 /**
- * Asynchronous queue listener. 
- * The messages produced by rabbitmq producer and intended to be handled in asynchronous way are consumed by this consumer.
+ * Asynchronous queue listener. The messages produced by rabbitmq producer and
+ * intended to be handled in asynchronous way are consumed by this consumer.
  * 
  * @author tsudake.psl@dlavideo.com
- *
+ * 
  */
 public class AsyncQueueConsumer implements Runnable {
 
@@ -30,11 +31,14 @@ public class AsyncQueueConsumer implements Runnable {
 	private QueueingConsumer consumer;
 	private Updater updater;
 	private QueueConfig myConfig;
+	private ObjectMapper mapper;
 
-	public AsyncQueueConsumer(QueueConfig config, Updater updater) throws IOException {
+	public AsyncQueueConsumer(QueueConfig config, Updater updater)
+			throws IOException {
 		this.updater = updater;
 		this.myConfig = config;
 
+		mapper = new ObjectMapper();
 		factory = new ConnectionFactory();
 		factory.setHost(myConfig.getRabbitMQServer());
 		factory.setPort(myConfig.getRabbitMQPort());
@@ -48,7 +52,8 @@ public class AsyncQueueConsumer implements Runnable {
 		asyncChannel.basicQos(1);
 		consumer = new QueueingConsumer(asyncChannel);
 		asyncChannel.basicConsume(myConfig.getName(), false, consumer);
-		logger.info("Started ASync queue listener, bound using key: " + myConfig.getBind_key());
+		logger.info("Started ASync queue listener, with updater: "
+				+ updater.getClass().getCanonicalName());
 	}
 
 	@Override
@@ -58,21 +63,25 @@ public class AsyncQueueConsumer implements Runnable {
 			try {
 				delivery = consumer.nextDelivery();
 				byte[] obj = delivery.getBody();
-				Event fe = Event.fromBytes(obj);
-				//Write to an endpoint (such as Cassandra, ElasticSearch, PredictionIO etc.)
+				Event fe = Event.fromJson(mapper, obj);
+				// Write to an endpoint (such as Cassandra, ElasticSearch,
+				// PredictionIO etc.)
+
 				updater.updateAsyncEvent(fe);
-				//Default acknowledgment
+				// Default acknowledgment
 				try {
-					asyncChannel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+					asyncChannel.basicAck(delivery.getEnvelope()
+							.getDeliveryTag(), false);
 				} catch (IOException e) {
 					logger.error(e.getMessage(), e);
 				}
 			} catch (Exception e) {
 				logger.error(e.getMessage(), e);
 				try {
-					//Send Negative ACK to broker in case of exception
-					//TODO Forward rejected message to dead letter exchange
-					asyncChannel.basicNack(delivery.getEnvelope().getDeliveryTag(), false, false);
+					// Send Negative ACK to broker in case of exception
+					// TODO Forward rejected message to dead letter exchange
+					asyncChannel.basicNack(delivery.getEnvelope()
+							.getDeliveryTag(), false, false);
 				} catch (IOException e1) {
 					logger.error(e1.getMessage(), e1);
 				}
--- spark-apps/data-sync-consumer/src/main/java/com/dla/foundation/intelligence/eo/consumer/SyncQueueConsumer.java
+++ spark-apps/data-sync-consumer/src/main/java/com/dla/foundation/intelligence/eo/consumer/SyncQueueConsumer.java
@@ -8,6 +8,7 @@ import com.dla.foundation.data.entities.event.Event;
 import com.dla.foundation.intelligence.eo.updater.Updater;
 import com.dla.foundation.intelligence.eo.util.BlockedListenerLogger;
 import com.dla.foundation.intelligence.eo.util.QueueListenerConfigHandler.QueueConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -15,11 +16,11 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
 
 /**
- * Synchronous queue listener. 
- * The messages produced by rabbitmq producer and intended to be handled in synchronous way are consumed by this consumer.
+ * Synchronous queue listener. The messages produced by rabbitmq producer and
+ * intended to be handled in synchronous way are consumed by this consumer.
  * 
  * @author tsudake.psl@dlavideo.com
- *
+ * 
  */
 public class SyncQueueConsumer implements Runnable {
 
@@ -31,11 +32,14 @@ public class SyncQueueConsumer implements Runnable {
 	private QueueingConsumer consumer;
 	private Updater updater;
 	private QueueConfig myConfig;
+	private ObjectMapper mapper;
 
-	public SyncQueueConsumer(QueueConfig config, Updater updater) throws IOException {
+	public SyncQueueConsumer(QueueConfig config, Updater updater)
+			throws IOException {
 		this.updater = updater;
 		this.myConfig = config;
 
+		mapper = new ObjectMapper();
 		factory = new ConnectionFactory();
 		factory.setHost(myConfig.getRabbitMQServer());
 		factory.setPort(myConfig.getRabbitMQPort());
@@ -49,7 +53,8 @@ public class SyncQueueConsumer implements Runnable {
 		syncChannel.basicQos(1);
 		consumer = new QueueingConsumer(syncChannel);
 		syncChannel.basicConsume(myConfig.getName(), false, consumer);
-		logger.info("Started Sync queue listener, bound using key: " + myConfig.getBind_key());
+		logger.info("Started Sync queue listener, with updater: "
+				+ updater.getClass().getCanonicalName());
 	}
 
 	@Override
@@ -62,26 +67,32 @@ public class SyncQueueConsumer implements Runnable {
 			try {
 				delivery = consumer.nextDelivery();
 				props = delivery.getProperties();
-				replyProps = new BasicProperties.Builder().correlationId(props.getCorrelationId()).build();
+				replyProps = new BasicProperties.Builder().correlationId(
+						props.getCorrelationId()).build();
 
 				byte[] obj = delivery.getBody();
-				Event fe = Event.fromBytes(obj);
-				//Write to an endpoint (such as Cassandra, ElasticSearch, PredictionIO etc.)
+				Event fe = Event.fromJson(mapper, obj);
+
+				// Write to an endpoint (such as Cassandra, ElasticSearch,
+				// PredictionIO etc.)
 				fe = (Event) updater.updateSyncEvent(fe);
-				//Push reply message to reply queue defined by producer.
-				syncChannel.basicPublish("", props.getReplyTo(), replyProps, fe.toBytes());
+				// Push reply message to reply queue defined by producer.
+				syncChannel.basicPublish("", props.getReplyTo(), replyProps,
+						fe.toBytes());
 				try {
-					//Default acknowledgment
-					syncChannel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+					// Default acknowledgment
+					syncChannel.basicAck(delivery.getEnvelope()
+							.getDeliveryTag(), false);
 				} catch (IOException e) {
 					logger.error(e.getMessage(), e);
 				}
 			} catch (Exception e) {
 				logger.error(e.getMessage(), e);
 				try {
-					//Send Negative ACK to broker in case of exception
-					//TODO Forward rejected message to dead letter exchange
-					syncChannel.basicNack(delivery.getEnvelope().getDeliveryTag(), false, true);
+					// Send Negative ACK to broker in case of exception
+					// TODO Forward rejected message to dead letter exchange
+					syncChannel.basicNack(delivery.getEnvelope()
+							.getDeliveryTag(), false, true);
 				} catch (IOException e1) {
 					logger.error(e1.getMessage(), e1);
 				}
